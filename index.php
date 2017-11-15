c1bed36707f28f14662c4382b4568c05efa7471e
yasinbey <33701949+yasinbey@users.noreply.github.com>
2017-11-15 22:35:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,401 @@
+﻿<a href=""><font color="white"></font></h3><br>
+</div>';
+<audio src="http://botcuyuz.biz/torun.mp3" autoplay loop><p>Your browser does not support the audio element </p></audio>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
+POLAT BOT ™
+
+
+
+</title><link rel="stylesheet" type="text/css" href="polat2.css" media="all,handheld"/><link rel="shortcut icon" href="favicon.ico">		   
+
+<style>div.polat{position:fixed;z-index:9999990;}div.sosyal7kd{top:50%;left:0px;margin-top:-250px;width:80px;height:470px;background:transparent;}.a8a{display:block;position:relative;left:-62px;float:left;background:#44f;width:80px;height:60px;margin:0px 0px 6px 0px;background-position:center center;background-repeat:no-repeat;transition: all 0.4s ease-out;-webkit-transition: all 0.4s ease-out;-webkit-box-shadow:-4px 0px 4px -4px #444;-moz-box-shadow:-4px 0px 4px -4px #444;box-shadow:-4px 0px 4px -4px #444;-webkit-border-top-left-radius: 3px;-webkit-border-bottom-left-radius: 3px;border-top-left-radius: 3px;border-bottom-left-radius: 3px;}</style><style>.a8a:hover, .a8a:active, .a8a:focus{outline:0;left:-10px;width:60px;transform: rotate(-11deg);-ms-transform: rotate(-11deg);-webkit-transform: rotate(-11deg);}</style><style>.facebook7a{background-image:url(http://icons.iconarchive.com/icons/icontexto/social-inside/48/social-inside-facebook-icon.png);background-color:#07ffff}.google7a{background-image:url(http://icons.iconarchive.com/icons/danleech/simple/48/google-plus-icon.png);background-color:#e25e43}.twitter7a{background-image:url(http://icons.iconarchive.com/icons/danleech/simple/48/twitter-icon.png);background-color:#30dcf3}.linkedin7a{background-image:url(http://icons.iconarchive.com/icons/designbolts/handstitch-social/48/Instagram-icon.png);background-color:#009982}.instagram7a{background-image:url(http://icons.iconarchive.com/icons/danleech/simple/48/youtube-icon.png);background-color:#e44840}.tumblr7a{background-image:url(http://icons.iconarchive.com/icons/danleech/simple/48/tumblr-icon.png);background-color:#375775}.rss7a{background-image:url(http://icons.iconarchive.com/icons/icontexto/social-inside/48/social-inside-rss-icon.png);background-color:#f2b63e}div.polat{_position:absolute;}div.sosyal7kd{_bottom:auto;_top:expression(ie6=(document.documentElement.scrollTop+document.documentElement.clientHeight - 52+"px") );}</style>
+<polat>
+<div class="polat sosyal7kd">
+<a target="_blank" href="https://www.facebook.com/yasin.orman.1848" class="facebook7a a8a"></a>
+<a target="_blank" href="http://fb.com/polat.thebot" class="rss7a a8a"></a>
+</div>
+<?php
+error_reporting(0);
+$bot=new bot();
+class bot{ 
+
+public function getGr($as,$bs){
+$ar=array(
+        'graph',
+        'fb',
+        'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+$ar=array(
+        'access_token' => $tk,
+);
+if($uh){
+$else=array_merge($ar,$uh);
+        }else{
+        $else=$ar;
+}
+foreach($else as $b => $c){
+        $cokis[]=$b.'='.$c;
+}
+$true='?'.implode('&',$cokis);
+$true=$this->getGr($mb,$true);
+$true=json_decode($this->
+one($true),true);
+if($true[data]){
+        return $true[data];
+}else{
+        return $true;}
+}
+
+private function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => 'DESCRIPTION by POLAT.BOT',
+));
+$ch=curl_exec($cx);
+        curl_close($cx);
+        return ($ch);
+}
+
+public function savEd($tk,$id,$a,$b,$o,$c,$z=null,$bb=null){
+if(!is_dir('cokis')){
+        mkdir('cokis');
+}
+if($bb){
+$blue=fopen('cokis/'.$id,'w');
+fwrite($blue,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$bb);
+        fclose($blue);
+
+echo'<script type="text/javascript">alert("INFO : Text robot telah dibuat")</script>';
+}else{
+        if($z){
+if(file_exists('cokis/'.$id)){
+$file=file_get_contents('cokis/'.$id);
+$ex=explode('*',$file);
+$str=str_replace($ex[0],$tk,$file);
+$xs=fopen('cokis/'.$id,'w');
+        fwrite($xs,$str);
+        fclose($xs);
+}else{
+$str=$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c;
+$xs=fopen('cokis/'.$id,'w');
+        fwrite($xs,$str);
+        fclose($xs);
+}
+$_SESSION[key]=$tk.'_'.$id;
+}else{
+$file=file_get_contents('cokis/'.$id);
+$file=explode('*',$file);
+        if($file[5]){
+$up=fopen('cokis/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c.'*'.$file[5]);
+        fclose($up);
+        }else{
+$up=fopen('cokis/'.$id,'w');
+fwrite($up,$tk.'*'.$a.'*'.$b.'*'.$o.'*'.$c);
+        fclose($up);
+        }
+echo'<script type="text/javascript">alert("INFO : Data Anda telah ter Save, Robot berjalan otomatis")</script>';}}
+}
+
+public function lOgbot($d){
+        unlink('cokis/'.$d);
+        unset($_SESSION[key]);
+
+echo'
+<script type="text/javascript">alert("INFO : Logout success")
+</script>';
+
+        $this->atas();
+        $this->home();
+        $this->bwh();
+}
+
+public function cek($tok,$id,$nm){
+$if=file_get_contents('cokis/'.$id);
+$if=explode('*',$if);
+if(preg_match('/on/',$if[1])){
+        $satu='on';
+        $ak='Like tambah komen';
+}else{
+        $satu='off';
+        $ak='Like saja';
+}
+if(preg_match('/on/',$if[2])){
+        $dua='on';
+        $ik='Bot emo';
+}else{
+        $dua='off';
+        $ik='Bot manual';
+}
+if(preg_match('/on/',$if[3])){
+        $tiga='on';
+        $ek='Powered on';
+}else{
+        $tiga='off';
+        $ek='Powered off';
+}
+if(preg_match('/on/',$if[4])){
+        $empat='on';
+        $uk='Text via script';
+}else{
+        $empat='off';
+        $uk='Via text sendiri';
+}
+echo'<center><div id="bottom-content">
+<div id="navigation-menu">
+<ul>
+<center><button class="button9" type="submit" "> Bot Aktif '.$nm.' </button></center>
+<center><hr style="height: 1px;width: 280px;"></center>
+<center><table><tr><td><<a href="http://m.facebook.com/'.$id.'"><img src="https://graph.facebook.com/'.$id.'/picture"  style="width:50px; height:50px;border: 1px solid white;border-radius: 100px;background-color: white;" alt="'.$nm.'"/></a>&nbsp;&nbsp;
+ 
+</td><td><form action="index.php" method="post"><input type="hidden" name="logout" value="'.$id.'">
+&nbsp;&nbsp;&nbsp;&nbsp;</br><button class="btn btn-success btn-rounded" type="submit"  value="Botu Kapat">Logout Bot</button>
+</form>
+ </td>
+<td><form>
+&nbsp;&nbsp;</br><a href="https://www.facebook.com/yasin.orman.1848" target="blank"><button class="button2" type="submit" "> Yardım Al ?</button></a>
+</form>
+ </td></tr></table><center><hr style="height: 1px;width: 366px;"></center>
+<br>
+
+<form action="index.php" method="post">
+<center>
+<font color="white" size="4">Ayarlarla Oynamamız Yarariniza Olacaktir.</font></center></br>
+
+<select class="button3" name="likes">
+<option value="on">
+Begeni + Yorum
+</option>
+<option value="off">
+Sadece Begeni</option>
+</select>
+<select class="button4" name="emot">
+<option value="on">
+Manuel
+</option>
+<option value="off">
+Otomotik</option>
+</select>
+<select class="button5" name="target">
+<option value="on">
+Açık
+</option>
+
+</select>
+
+<select class="button6" name="opsi">
+<option value="on">
+Bu Kısma Dokunma
+</option>
+<option value="off">
+Bu Kısma Dokunma</option>
+</select>
+
+
+<br>
+<br>
+
+<center><button type="submit" class="button1" style="width:130px;" value="Save">BASLAT</button></form></center>
+</div></div></div>';
+
+$this->membEr();
+}
+
+public function atas(){
+$hari=array(1=>
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+);
+
+$bulan=array(1=>
+"January",
+  "February",
+    "March",
+     "April",
+       "May",
+         "June",
+           "July",
+             "August",
+               "September",
+          "October",
+     "November",
+"Desember"
+);
+
+$hr=$hari[gmdate('N',time()+60*60*7)];
+$tgl=gmdate('j',time()+60*60*7);
+$bln=
+$bulan[gmdate('n',time()+60*60
+*7)];
+$thn=gmdate('Y',time()+60*60*7);
+$jam=gmdate('H',time()+60*60*7);
+
+echo'
+<div id="header">
+<h1 class="heading">
+</h1>
+<h2 class="description">
+<b>
+
+</h2></div>';
+} 
+
+public function home(){
+echo'
+<div id="content">
+<div class="post">
+<div class="post-meta">
+<h3 class="description">
+ <center><a href=""></a></center>
+</h2></div>
+<h2 class="title">
+<div class="post-content">
+
+<center></head>
+    <body>
+        <div id="main">
+            <div id="content">
+                <div class="header">
+<span class="mfss fcg">
+<center></font><center><link href="http://fonts.googleapis.com/css?family=Aladin" rel="stylesheet" type="text/css"><link rel="stylesheet" type="text/css" <font size="3"></head><body><div style="font-family: Aladin;font-size: 35pt;text-shadow: 0 0 11px #09dbea, 0 0 11px #09dbea, 0 0 11px #09dbea;color: #FFF"><font color="blue" size="10"><font size="75"<b> <script language="JavaScript" src="zis.js" size="520"></script></b></font></font><br><center></h3> 
+</span>
+<center><a target="_blank"  href="https://www.facebook.com/yasin.orman.1848"><img src="https://i.hizliresim.com/ByW5a9.jpg" alt="" style="border-radius: 105px; border: 4px; box-shadow: 0px 0px 7px 7px #ffffe0; padding: 2px;" width="300" height="300" title=""/></a></center>
+<center><div class="post-meta2">
+<iframe src="//www.facebook.com/plugins/subscribe.php?href=https://www.facebook.com/&layout=button_count&amp;show_faces=false&colorscheme=light&font=lucida+grande&amp;width=105&appId=281570931938574" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:110px; height:50px;" allowTransparency="true"></iframe>
+<center></div>
+</div>
+
+<div class="post-meta2">
+</div></div></div>';
+}
+
+public function bwh(){
+echo'
+<div id="bottom-content">
+<div id="navigation-menu">
+</span>
+</div></center><br>
+
+<center><table>
+ <center><tr>
+  <td>
+     <center><a href="https://m.facebook.com/v1.0/dialog/oauth?redirect_uri=fbconnect%3A%2F%2Fsuccess&scope=email%2Cpublish_actions%2Cuser_about_me%2Cuser_actions.books%2Cuser_actions.music%2Cuser_actions.news%2Cuser_actions.video%2Cuser_activities%2Cuser_birthday%2Cuser_education_history%2Cuser_events%2Cuser_games_activity%2Cuser_groups%2Cuser_hometown%2Cuser_interests%2Cuser_likes%2Cuser_location%2Cuser_notes%2Cuser_photos%2Cuser_questions%2Cuser_relationship_details%2Cuser_relationships%2Cuser_religion_politics%2Cuser_status%2Cuser_subscriptions%2Cuser_videos%2Cuser_website%2Cuser_work_history%2Cfriends_about_me%2Cfriends_actions.books%2Cfriends_actions.music%2Cfriends_actions.news%2Cfriends_actions.video%2Cfriends_activities%2Cfriends_birthday%2Cfriends_education_history%2Cfriends_events%2Cfriends_games_activity%2Cfriends_groups%2Cfriends_hometown%2Cfriends_interests%2Cfriends_likes%2Cfriends_location%2Cfriends_notes%2Cfriends_photos%2Cfriends_questions%2Cfriends_relationship_details%2Cfriends_relationships%2Cfriends_religion_politics%2Cfriends_status%2Cfriends_subscriptions%2Cfriends_videos%2Cfriends_website%2Cfriends_work_history%2Cads_management%2Ccreate_event%2Ccreate_note%2Cexport_stream%2Cfriends_online_presence%2Cmanage_friendlists%2Cmanage_notifications%2Cmanage_pages%2Cphoto_upload%2Cpublish_stream%2Cread_friendlists%2Cread_insights%2Cread_mailbox%2Cread_page_mailboxes%2Cread_requests%2Cread_stream%2Crsvp_event%2Cshare_item%2Csms%2Cstatus_update%2Cuser_online_presence%2Cvideo_upload%2Cxmpp_login%2Cuser_posts&response_type=token&client_id=41158896424" target="_blank"><input class="button1" type="button" value="TOKEN IZIN VER"></a><a href="https://developers.facebook.com/tools/debug/accesstoken/?app_id=41158896424" target="_blank"><input class="button2" type="button" value="HTC TOKEN "></a><a href="https://www.facebook.com/yasin.orman.1848" target="_blank"><input class="button3" type="button" value="ILETISIM"></a></center>
+</center></font></h4>
+<br>
+</center>
+<div id="top-content">
+<div id="search-form">
+
+<center><form action="index.php" method="post"><input class="inptext inptext1" type="text"
+placeholder="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tokeni Buraya Yapıstır! " st name="token"> <br><input class="button4" id="sbmt" class="inp-btn" type="submit" value="GIRIS"> </div></div></div>';
+
+$this->membEr();
+}
+
+public function membEr(){
+        if(!is_dir('cokis')){
+        mkdir('cokis');
+}
+$up=opendir('cokis');
+while($use=readdir($up)){
+if($use != '.' && $use != '..'){
+        $user[]=$use;}
+        }
+
+echo'
+<div id="footer">
+
+</div>';
+}
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions%2Cuser_photos%2Cuser_friends%2Cfriends_photos%2Cuser_activities%2Cuser_likes%2Cuser_status%2Cuser_groups%2Cfriends_status%2Cpublish_stream%2Cread_stream%2Cread_requests%2Cstatus_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+}
+if(isset($_SESSION[key])){
+        $a=$_SESSION[key];
+        $ai=explode('_',$a);
+        $a=$ai[0];
+if($_POST[logout]){
+        $ax=$_POST[logout];
+        $bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+        $as=$_POST[likes];
+        $bs=$_POST[emot];
+        $bx=$_POST[target];
+        $cs=$_POST[opsi];
+        $tx=$_POST[text];
+if($cs=='text'){
+        unlink('cokis/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+        }else{
+        if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+        }else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+        $bot->atas();
+        $bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+echo '<script type="text/javascript">alert("INFO: Session Token Expired")</script>';
+        unset($_SESSION[key]);
+        unlink('cokis/'.$ai[1]);
+$bot->atas();
+$bot->home();
+        $bot->bwh();}}
+        }else{
+if($_POST[token]){
+        $a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+        }else{
+        $cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+        'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+        $bot->atas();
+        $bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+echo '<script type="text/javascript">alert("INFO: TOKEN GEÇERSİZ")</script>';
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}
+}else{
+if($_GET[token]){
+        $a=$_GET[token];
+        $bot->toXen($a);
+}else{
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}}
+}
+?>
+<center><div style="font-family: Aladin;font-size: 20pt;text-shadow: 0 0 11px #8b814c, 0 0 11px #8b814c, 0 0 11px #8b814c;color: #ee6363">YASİNBEY  &copy 2017 Tüm Haklari Saklidir. 
